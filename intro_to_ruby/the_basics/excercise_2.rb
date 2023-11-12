ae5033db668f11d1d86f575bb8ed37011f769988
Nikko Macatangay <nikkomacatangay@gmail.com>
2023-11-12 15:13:43 +0000

    The basics exercises completed

---
+++ intro_to_ruby/the_basics/excercise_2.rb
@@ -0,0 +1,6 @@
+thousands = 5684 / 1000
+hundreds = 5684 / 100 % 10
+tens = 5684 / 10 % 10
+ones = 5864 % 10
+
+puts thousands, hundreds, tens, ones
---
+++ intro_to_ruby/the_basics/excercise_3.rb
@@ -0,0 +1,12 @@
+movies = {:night_child => 1975,
+          :walking_tall => 2004,
+          :man_of_steel => 2013,
+          :out_cold => 2001,
+          :the_survivor => 1981}
+
+puts movies[:night_child]
+puts movies[:walking_tall]
+puts movies[:man_of_steel]
+puts movies[:out_cold]
+puts movies[:the_survivor]
+
